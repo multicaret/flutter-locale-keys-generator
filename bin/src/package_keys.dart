8821c4d7ad01d2cecc257ed17b80944b4afa6a21
suhailkawsara <msk286@gmail.com>
2024-11-12 23:49:14 +0300

    Refactor path construction for pubspec.yaml location
    This updates the code to use the current working directory instead of the script directory for locating the pubspec.yaml file. This change ensures the path is constructed relative to the project structure for better consistency and reliability.
    
    Signed-off-by: suhailkawsara <msk286@gmail.com>

--- bin/src/package_keys.dart
+++ bin/src/package_keys.dart
@@ -10,11 +10,12 @@ class PackageKeys {
   late String name;
 
   PackageKeys() {
-    // Determine the directory of the current script
-    final scriptDirectory = path.dirname(Platform.script.toFilePath());
+    // Get the current directory
+    final String currentDir = Directory.current.path;
 
-    // Construct the path to the yaml file relative to the current script
-    final yamlPath = path.join(scriptDirectory, 'pubspec.yaml');
+    // Construct the path to the pubspec.yaml file relative to the current working directory
+    final String yamlPath = path.join(
+        currentDir, 'lib', 'current_package', 'pubspec.yaml'); // Adjust the path accordingly
 
     // Read the yaml file
     final String pubspec = File(yamlPath).readAsStringSync();
